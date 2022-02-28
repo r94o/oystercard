abcaa5ea108c183207902d8de8cac67f42e0fa8c
Rob Oman <rob.oman@icloud.com>
2022-02-28 17:26:59 +0000

    Add maximum balance and top_up will raise error if exceeded
    Co-authored-by: Henry Ferrari <HCVF@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,22 @@
 class Oystercard
 
   attr_reader :balance
+
+  MAXIMUM_BALANCE = 90
   
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Max balance is £#{MAXIMUM_BALANCE}" if will_exceed_max_balance?(amount)
     @balance += amount
   end
+
+  private
+
+  def will_exceed_max_balance?(amount)
+    @balance + amount > MAXIMUM_BALANCE
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  MAXIMUM_BALANCE = Oystercard::MAXIMUM_BALANCE
   describe "#balance" do
     it "starts with a balance of 0" do
       expect(subject.balance).to eq 0
@@ -10,6 +11,10 @@ describe Oystercard do
   describe "#top_up" do
     it "adds 50 to the balance and returns the new balance" do  
       expect(subject.top_up(50)).to eq(50)
+    end
+    it "raises an error when the balance exceeds #{MAXIMUM_BALANCE}" do
+      subject.top_up(MAXIMUM_BALANCE)
+      expect { subject.top_up(1) }.to raise_error("Max balance is £#{MAXIMUM_BALANCE}")
     end
   end
 end
