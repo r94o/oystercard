893be120e9b2e65cd6840268b205902095b5c2ec
Rob Oman <rob.oman@icloud.com>
2022-02-28 16:18:08 +0000

    New Oystercards initiate with a balance of 0
    Co-authored-by: Henry Ferrari <HCVF@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,13 @@
 class Oystercard
 
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def wallet_empty?
+    @balance == 0
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
 require 'oystercard'
 
+
 describe Oystercard do
+  it "starts with a balance of 0" do
+    expect(subject.wallet_empty?).to eq true
+  end
+  it "starts with a balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 end
+
