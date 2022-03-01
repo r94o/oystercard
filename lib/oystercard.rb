b84602a7a2a0988df3d2e23f0b0aa2d3800c1cf2
Henry <hcvferrari@gmail.com>
2022-03-01 20:32:19 +0000

    Added journey history which records a hash with journey start time as key and start/end journeys as values
    Co-authored-by: Rob Oman <r94o@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :journey_history
 
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
   
   def initialize
     @balance = 0
+    @journey_history = []
   end
 
   def top_up(amount)
@@ -17,20 +18,35 @@ class Oystercard
   def touch_in(station)
     fail "You have already touched in" if in_journey?
     fail "You need to top up" if @balance < MINIMUM_FARE
-    @entry_station = station
+    log_start_journey(station)
   end
 
-  def touch_out
+  def touch_out(station)
     fail "You have not touched in" unless in_journey?
     deduct(MINIMUM_FARE)
-    @entry_station = nil
+    log_end_journey(station)
   end
 
   def in_journey?
-    @entry_station
+    !!@start_time
   end
 
   private
+
+  def log_start_journey(station)
+    @start_time = Time.now
+    @this_journey = { @start_time => [station]}
+  end
+
+  def log_end_journey(station)
+    @this_journey[@start_time] << station
+    @start_time = nil
+    save_journey
+  end
+
+  def save_journey
+    @journey_history << @this_journey
+  end
 
   def will_exceed_max_balance?(amount)
     @balance + amount > MAXIMUM_BALANCE
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,20 @@
 require 'oystercard'
 
 describe Oystercard do
-  let(:station){ double :station }
+  let(:entry_station){ double :station }
+  let(:exit_station){ double :station }
   MAXIMUM_BALANCE = Oystercard::MAXIMUM_BALANCE
   MINIMUM_FARE = Oystercard::MINIMUM_FARE
   
   describe "#balance" do
     it "starts with a balance of 0" do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  describe "#journey_history" do
+    it "starts as an empty array" do
+      expect(subject.journey_history).to be_empty
     end
   end
 
@@ -27,33 +34,41 @@ describe Oystercard do
 
     describe "#touch_in" do
       it "can touch in" do
-        subject.touch_in(station)
+        subject.touch_in(entry_station)
         expect(subject).to be_in_journey
       end
       it "raises error if #touch_in twice without #touch_out" do
-        subject.touch_in(station)
-        expect { subject.touch_in(station) }.to raise_error("You have already touched in")
-      end
-      it "remembers the entry station" do
-        subject.touch_in(station)
-        expect(subject.entry_station).to be(station)
+        subject.touch_in(entry_station)
+        expect { subject.touch_in(entry_station) }.to raise_error("You have already touched in")
       end
     end
 
     describe "#touch_out" do
       it "can touch out" do
-        subject.touch_in(station)
-        subject.touch_out
+        subject.touch_in(entry_station)
+        subject.touch_out(exit_station)
         expect(subject).to_not be_in_journey
       end
-      it "changes entry_station to nil" do
-        subject.touch_in(station)
-        subject.touch_out
-        expect(subject.entry_station).to be_nil
+      it "reduces balance by #{MINIMUM_FARE}" do
+        subject.touch_in(entry_station)
+        expect { subject.touch_out(exit_station) }.to change{ subject.balance }.by(-MINIMUM_FARE)
       end
-      it "reduce balance by #{MINIMUM_FARE}" do
-        subject.touch_in(station)
-        expect { subject.touch_out }.to change{ subject.balance }.by(-MINIMUM_FARE)
+      it "logs the journey" do
+        allow(Time).to receive(:now).and_return(:start_time)
+        subject.touch_in(entry_station)
+        expect(subject.touch_out(exit_station)).to eq([{start_time: [entry_station, exit_station]}])
+      end
+    end
+    
+    describe "#in_journey?" do
+      it "is in a journey once touched in" do
+        subject.touch_in(entry_station)
+        expect(subject).to be_in_journey
+      end
+      it "is not in a journey once touched out" do
+        subject.touch_in(entry_station)
+        subject.touch_out(exit_station)
+        expect(subject).to_not be_in_journey
       end
     end
   end
@@ -61,14 +76,14 @@ describe Oystercard do
   context "balance is below #{MINIMUM_FARE}" do
     describe "#touch_in" do 
       it "raises error" do
-        expect { subject.touch_in(station) }.to raise_error("You need to top up")
+        expect { subject.touch_in(entry_station) }.to raise_error("You need to top up")
       end
     end
   end
 
   describe "#touch_out" do
     it "raises error if #touch_out without #touch_in" do
-      expect { subject.touch_out }.to raise_error("You have not touched in")
+      expect { subject.touch_out(exit_station) }.to raise_error("You have not touched in")
     end
   end
 
