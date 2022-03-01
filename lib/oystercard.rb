e310444edff55a543989c475105ade09ca92b259
Henry <hcvferrari@gmail.com>
2022-03-01 18:15:18 +0000

    Updated #touch_in to set entry station, and #touch_out to set it to nil
    Co-authored-by: Rob Oman <r94o@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,12 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
   
   def initialize
     @balance = 0
-    @in_journey = false
   end
 
   def top_up(amount)
@@ -15,20 +14,20 @@ class Oystercard
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station)
     fail "You have already touched in" if in_journey?
     fail "You need to top up" if @balance < MINIMUM_FARE
-    @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
     fail "You have not touched in" unless in_journey?
     deduct(MINIMUM_FARE)
-    @in_journey = false
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_journey
+    @entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station){ double :station }
   MAXIMUM_BALANCE = Oystercard::MAXIMUM_BALANCE
   MINIMUM_FARE = Oystercard::MINIMUM_FARE
   
@@ -26,23 +27,32 @@ describe Oystercard do
 
     describe "#touch_in" do
       it "can touch in" do
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject).to be_in_journey
       end
       it "raises error if #touch_in twice without #touch_out" do
-        subject.touch_in
-        expect { subject.touch_in }.to raise_error("You have already touched in")
+        subject.touch_in(station)
+        expect { subject.touch_in(station) }.to raise_error("You have already touched in")
+      end
+      it "remembers the entry station" do
+        subject.touch_in(station)
+        expect(subject.entry_station).to be(station)
       end
     end
 
     describe "#touch_out" do
       it "can touch out" do
-        subject.touch_in
+        subject.touch_in(station)
         subject.touch_out
         expect(subject).to_not be_in_journey
       end
+      it "changes entry_station to nil" do
+        subject.touch_in(station)
+        subject.touch_out
+        expect(subject.entry_station).to be_nil
+      end
       it "reduce balance by #{MINIMUM_FARE}" do
-        subject.touch_in
+        subject.touch_in(station)
         expect { subject.touch_out }.to change{ subject.balance }.by(-MINIMUM_FARE)
       end
     end
@@ -51,7 +61,7 @@ describe Oystercard do
   context "balance is below #{MINIMUM_FARE}" do
     describe "#touch_in" do 
       it "raises error" do
-        expect { subject.touch_in }.to raise_error("You need to top up")
+        expect { subject.touch_in(station) }.to raise_error("You need to top up")
       end
     end
   end
@@ -68,4 +78,3 @@ describe Oystercard do
     end
   end
 end
-
